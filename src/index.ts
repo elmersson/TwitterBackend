ec6c9f2b4daeb1e9b169c5c4ee633d2549cf77d2
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-07-07 13:38:14 +0200

    added-basic-layout

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,34 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
+// create user
+app.post('/user', (req, res) => {
+    res.status(501).json({error: "Not implemented"})
+});
+
+// list users
+app.get('/user', (req, res) => {
+    res.status(501).json({error: "Not implemented"})
+});
+
+// get one user
+app.get('/user/:id', (req, res) => {
+    const {id} = req.params;
+    res.status(501).json({error: `Not implemented: ${id}`})
+});
+
+// update user
+app.put('/user/:id', (req, res) => {
+    const {id} = req.params;
+    res.status(501).json({error: `Not implemented: ${id}`})
+});
+
+// delete user
+app.delete('/user/:id', (req, res) => {
+    const {id} = req.params;
+    res.status(501).json({error: `Not implemented: ${id}`})
+});
+
 app.listen(3000, () => {
     console.log("Server ready at localhost:3000")
 })
